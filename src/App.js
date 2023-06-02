c9b249bd39957b661acdbc6843fb00560f4ac279
Hamza Husain <mhusain3@illinois.edu>
2023-06-02 12:03:19 +0530

    only P1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Home from './componenets/Home';
+// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+// import Home from './componenets/Home';
 import P1 from './componenets/P1';
-import P2 from './componenets/P2';
-import Navigation from './componenets/Navigation';
+// import P2 from './componenets/P2';
+// import Navigation from './componenets/Navigation';
 
 function App() {
   return (
