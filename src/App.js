8f21d7608decea7ac2c80e027568edeeb4967bb0
Hamza Husain <mhusain3@illinois.edu>
2023-06-02 12:06:21 +0530

    added nav back

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-// import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-// import Home from './componenets/Home';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from './componenets/Home';
 import P1 from './componenets/P1';
-// import P2 from './componenets/P2';
-// import Navigation from './componenets/Navigation';
+import P2 from './componenets/P2';
+import Navigation from './componenets/Navigation';
 
 function App() {
   return (
     <div className="App">
-      <P1 />
-      {/* <Router>
+      <Router>
         <Navigation />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/P1" element={<P1 />} />
           <Route path="/P2" element={<P2 />} />
         </Routes>
-      </Router> */}
+      </Router>
     </div>
   );
 }
