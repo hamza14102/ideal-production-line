0be085dfbf76a1d09696f163036afc4380a4f135
Hamza Husain <mhusain3@illinois.edu>
2023-06-26 17:54:27 -0700

    added navbar auto-collapse

--- src/componenets/Navigation.js
+++ src/componenets/Navigation.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+
 const Navigation = () => {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <Navbar bg="light" expand="lg px-3">
+    <Navbar bg="light" expand="lg px-3" expanded={expanded}>
       <Navbar.Brand>
         <Link to="/" className="nav-link">
           Home
         </Link>
         {/* Home */}
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")} />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           {/* <Link to="/" className="nav-link">
@@ -20,10 +22,10 @@ const Navigation = () => {
           {/* <Link to="/P1" className="nav-link">
             Distribution Calculator
           </Link> */}
-          <Link to="/P2" className="nav-link">
+          <Link to="/P2" className="nav-link" onClick={() => setExpanded(false)}>
             Create Product
           </Link>
-          <Link to="/SupervisorSearch" className="nav-link">
+          <Link to="/SupervisorSearch" className="nav-link" onClick={() => setExpanded(false)}>
             Supervisor Search
           </Link>
         </Nav>
